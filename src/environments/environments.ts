bdadd78477e79e022c1c8af998e909b2e34c4956
Nirm0525 <nora.rivas@traveldiunsa.com>
2026-08-14 10:18:34 -0600

    Proyecto inicial Angular

---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  supabaseUrl: 'https://ejnatentupflnvjnklwu.supabase.co',
+
+  supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqbmF0ZW50dXBmbG52am5rbHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MTU5MTQsImV4cCI6MjEwMjI5MTkxNH0.RU5sZFRiy8fuab4BfiqVGa7fnr3rVgjkhvfTx-j3eiY'
+};
